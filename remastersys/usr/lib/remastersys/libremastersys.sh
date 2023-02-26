5a5128be41417e95ce46bd4375f6b06f730b6d1e
nerun <danieldiasr@gmail.com>
2023-02-26 11:11:37 -0300

    Updated settings, messages and translations. Added 'Settings Integrity Check' function.

--- remastersys/usr/lib/remastersys/libremastersys.sh
+++ remastersys/usr/lib/remastersys/libremastersys.sh
@@ -50,3 +50,81 @@ function find_bin(){
     find /usr/bin /usr/sbin -name "$1"
 }
 
+# If remastersys.conf is incorrect or missing, set standards
+function SettingsIntegrityCheck(){
+    if [ -f /etc/remastersys.conf ]; then
+        if [ "$BASEWORKDIR" = "" ]; then
+            BASEWORKDIR="/home"
+        fi
+
+        if [ "$WORKDIR" = "" ]; then
+            WORKDIR="/home/remastersys"
+        fi
+
+        if [ "$LIVEUSER" = "" ]; then
+            LIVEUSER="live"
+        fi
+
+        # live username in lowercase
+        LIVEUSER="$(echo $LIVEUSER | awk '{print tolower ($0)}')"
+        LIVEUSER_FULL_NAME="$LIVEUSER session user"
+
+        if [ "$LIVECDLABEL" = "" ]; then
+            LIVECDLABEL="Remastersys"
+        fi
+
+        if [ "$CUSTOMISO" = "" ]; then
+            CUSTOMISO="remastersys-livecd.iso"
+        fi
+
+        if [ $(echo "$ISOBOOT" | awk '{print toupper ($0)}') = "ISOLINUX" ]; then
+            ISOBOOT="ISOLINUX"
+        else
+            # Including: "" and "grub"
+            ISOBOOT="GRUB"
+        fi
+
+        if [ "$SQUASHFSOPTS" = "" ]; then
+            SQUASHFSOPTS="-no-recovery -always-use-fragments -b 1M -comp zstd"
+        fi
+
+        SHOWICON="$(echo $SHOWICON | awk '{print tolower ($0)}')"
+
+        if [[ "$SHOWICON" == @("0"|"no"|"n"|"false") ]]; then
+            SHOWICON="0"
+        else
+            # Including: "" and "1"
+            SHOWICON="1"
+        fi
+    else
+        echo """# Remastersys Global Configuration File.
+
+# This is the temporary working directory and won't be included on the CD/DVD.
+BASEWORKDIR=\"/home\"
+WORKDIR=\"/home/remastersys\"
+
+# Here you can add any other files or directories to be excluded from the live
+# file system. Separate each entry with a space.
+EXCLUDES=\"\"
+
+# Here you can change the Live CD/DVD username.
+LIVEUSER=\"live\"
+
+# Here you can change the name of the Live CD/DVD label.
+LIVECDLABEL=\"Remastersys\"
+
+# Here you can change the name of the ISO file that is created.
+CUSTOMISO=\"remastersys-livecd.iso\"
+
+# Here you can change the boot method for the live: GRUB or ISOLINUX.
+ISOBOOT=\"GRUB\"
+
+# Options for mksquashfs.
+# Default is: \"-no-recovery -always-use-fragments -b 1M -comp zstd\"
+SQUASHFSOPTS=\"-no-recovery -always-use-fragments -b 1M -comp zstd\"
+
+# Option to show install icon on live desktop in dist mode:
+# 1=yes or true, 0=no or false.
+SHOWICON=\"1\"""" > /etc/remastersys.conf
+    fi
+}
